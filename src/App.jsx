2b951a2d73690db8e7ff3d7be4113296564d589e
unknown <rafaelraysantossouza1@gmail.com>
2024-07-22 15:44:19 -0300

    Adicionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
+const icon = "src/assets/editar.png"
+const bin = "src/assets/bin.png"
 function App() {
   const [produtos, setProdutos] = useState([]);
   const [id, setId] = useState('');
@@ -24,7 +25,7 @@ function App() {
       .catch(error => {
         console.error('Erro ao carregar produtos:', error);
       });
-      setCarregando(false);
+    setCarregando(false);
   };
 
   const onClickAdicionarProduto = () => {
@@ -40,7 +41,7 @@ function App() {
     setNome(item?.nome);
     setDescricao(item?.descricao);
     setPreco(item?.preco);
-    setAbrir(true);
+    setAbrir(true)
   };
 
   const salvarProduto = async () => {
@@ -102,50 +103,55 @@ function App() {
   };
 
   return (
-    <div className="App flex flex-col gap-5 w-[1157px]">
-      <h1>Gestão de Produtos</h1>
-      <button className='w-[200px] bg-green-600' onClick={onClickAdicionarProduto}>
-        Adicionar Produto
-      </button>
+    <div className="App flex flex-col gap-5 w-[1157px] ">
+      <h1 className='mb-[40px]'>Gestão de Produtos</h1>
+      <a href="#adicionar">
+        <button className='w-[200px] bg-green-600 text-black ml-[956px]' onClick={onClickAdicionarProduto}>
+          <p>Adicionar Produto</p>
+        </button>
+      </a>
       {carregando ? (
         <p>Carregando...</p>
       ) : (
         <ul className='flex flex-col gap-2'>
           {produtos?.map((item, index) => (
-            <li className='border border-gray-600 p-4 rounded-lg flex flex-col items-start flex-wrap gap-5 justify-between' key={index}>
-              <p className='font-bold'>Nome: {item?.nome}</p>
-                <small>Descrição: {item?.descricao} | Preço: R$ {item?.preco}</small>
-                <small>Dt. de Criação: {item?.data_de_criacao}</small>
-              <div className='flex gap-3'>
-                <button onClick={() => onClickEditar(item)}>
-                  <p>Editar</p>
-                </button>
-                <button className='bg-red-700' onClick={() => deletarProduto(item.id)}>
-                 <p>Deletar</p>
+            <li className='border border-gray-600 p-4 rounded-lg flex flex-col items-start flex-wrap gap-5 justify-between ' key={index}>
+              <p className='font-bold text-[18px]'>Nome: {item?.nome}</p>
+              <small>Descrição: <span className='text-yellow-300'>{item?.descricao}</span> | Preço: <span className='text-lime-500'>R${item?.preco}</span></small>
+              <small>Dt. de Criação: {item?.data_de_criacao}</small>
+              <div id='teste' className='flex gap-3'>
+                <a href="#adicionar">
+                  <button className='bg-slate-700' onClick={() => onClickEditar(item)}>
+                    <img className='w-[30px]' src={icon} alt="icon-editar" />
+                  </button>
+                </a>
+                <button className='bg-red-500' onClick={() => deletarProduto(item.id)}>
+                  <img src={bin} alt="" />
                 </button>
               </div>
-              <hr/>
+              <hr />
             </li>
           ))}
         </ul>
       )}
       {abrir && (
-        <div className="modal flex gap-2 items-center ">
-          <h2>{id ? 'Editar Produto' : 'Adicionar Produto'}</h2>
+        
+        <div className="modal flex gap-2 items-center justify-center">
+          <h2 id='adicionar' className='text-lime-700'>{id ? 'Editar Produto' : 'Adicionar Produto'}</h2>
           <label>
             Nome:
-            <input className='outline-0 pl-[2px]' value={nome} onChange={(e) => setNome(e.target.value)} />
+            <input className='outline-0 pl-[2px] ' value={nome} onChange={(e) => setNome(e.target.value)} />
           </label>
           <label>
-            Descrição:
-            <input className='outline-0 pl-[2px]' value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+            Descrição: 
+            <input className='outline-0 pl-[2px] ' value={descricao} onChange={(e) => setDescricao(e.target.value)} />
           </label>
           <label>
             Preço:
-            <input  className='outline-0 pl-[2px]' type="number" value={preco} onChange={(e) => setPreco(e.target.value)} />
+            <input className='outline-0 pl-[2px] ' type="number" value={preco} onChange={(e) => setPreco(e.target.value)} />
           </label>
-          <button onClick={salvarProduto}>{id ? 'Salvar Alterações' : 'Adicionar Produto'}</button>
-          <button onClick={() => setAbrir(false)}>Cancelar</button>
+          <a href="#root"><button onClick={salvarProduto}>{id ? 'Salvar Alterações' : 'Adicionar Produto'}</button></a>
+          <a href="#root"><button onClick={() => setAbrir(false)}>Cancelar</button></a>
         </div>
       )}
     </div>
